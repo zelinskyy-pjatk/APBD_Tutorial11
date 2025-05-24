146cbc3fecd84bdd5ad174d67291b63b18e0b02c
DimaZelinskyy <dimazelinskiy1@gmail.com>
2025-05-24 18:54:51 +0200

    Initial Commit

---
+++ APBD_11/Data/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APBD_11.Data;
+
+public class DatabaseContext : DbContext
+{
+    
+    
+    protected DatabaseContext()
+    {
+    }
+
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
---
+++ APBD_11/Program.cs
@@ -0,0 +1,18 @@
+using APBD_11.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DatabaseContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+);
+builder.Services.AddScoped<IDbService, DbService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
