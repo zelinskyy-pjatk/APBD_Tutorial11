3cfc695fa078db1fe513626b5cb67439a14b4f6b
DimaZelinskyy <dimazelinskiy1@gmail.com>
2025-05-25 20:33:56 +0200

    Migrations Updated

---
+++ APBD_11/Migrations/20250524214930_SeedDoctor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APBD_11.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDoctor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Doctor",
+                columns: new[] { "DoctorId", "Email", "FirstName", "LastName" },
+                values: new object[] { 1, "daniel.saga@gmail.com", "Daniel", "Saga" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Doctor",
+                keyColumn: "DoctorId",
+                keyValue: 1);
+        }
+    }
+}
---
+++ APBD_11/Migrations/20250524221125_SeedMedicament.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APBD_11.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMedicament : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Medicament",
+                columns: new[] { "MedicamentId", "Description", "Name", "Type" },
+                values: new object[] { 1, "Take max 1 time daily.", "Ibuprofen ", "Nonsteroidal anti-inflammatory drug (NSAID)" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Medicament",
+                keyColumn: "MedicamentId",
+                keyValue: 1);
+        }
+    }
+}
